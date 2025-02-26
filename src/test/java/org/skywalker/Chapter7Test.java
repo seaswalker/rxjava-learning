f6d14e30f42f08141412ae9ad1cdd9c45bf08474
seaswalker <xsdwem7@hotmail.com>
2025-02-26 08:29:00 +0800

    retry

--- src/test/java/org/skywalker/Chapter7Test.java
+++ src/test/java/org/skywalker/Chapter7Test.java
@@ -1,11 +1,16 @@
 package org.skywalker;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.Emitter;
 import rx.Observable;
 
 import java.util.concurrent.TimeUnit;
 
 public class Chapter7Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Chapter7Test.class);
 
     @Test
     void testTimeout() throws InterruptedException {
@@ -13,10 +18,23 @@ public class Chapter7Test {
                 .just("E")
                 .delay(50, TimeUnit.MILLISECONDS)
                 .concatWith(Observable.<String>empty().delay(200, TimeUnit.MILLISECONDS))
-                .timeout(300, TimeUnit.MILLISECONDS)
+                .timeout(100, TimeUnit.MILLISECONDS)
                 .subscribe(System.out::println, System.out::println);
 
         TimeUnit.MILLISECONDS.sleep(500);
+    }
+
+    @Test
+    void testRetry() {
+        Observable.create(
+                        emitter -> {
+                            log.info("Creating Called");
+                            throw new IllegalStateException();
+                        },
+                        Emitter.BackpressureMode.ERROR
+                )
+                .retry(1)
+                .subscribe(System.out::println, System.out::println);
     }
 
 }
